e052ff5de611f66fccddb7c93b1b8d7b20978baa
Joshua A <joshuamathewj2@gmail.com>
2025-11-20 21:29:20 +0530

    Create db_connect.php

---
+++ includes/db_connect.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "joshmart";
+
+function connectDB() {
+    global $servername, $username, $password, $dbname;
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    return $conn;
+}
+
+function closeDB($conn) {
+    $conn->close();
+}
+?>
